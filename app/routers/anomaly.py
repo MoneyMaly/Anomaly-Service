caa0c7f143cc3291ddd97b334184e8e8f9ab0535
Nofar <nofar11sh@gmail.com>
2022-04-26 17:33:30 +0300

    first authenticatedAPI

---
+++ app/routers/anomaly.py
@@ -0,0 +1,21 @@
+from typing import Optional, List
+
+from fastapi import APIRouter, HTTPException, status, Depends
+
+from app.utils.auth_helper import JWTBearer
+
+router = APIRouter(tags=['Anomaly Detector'])
+
+credentials_exception = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Could not validate credentials",
+    headers={"WWW-Authenticate": "Bearer"})
+
+
+@router.get("/users/{username}/bankaccounts/balance/",status_code=status.HTTP_200_OK, dependencies=[Depends(JWTBearer())])
+async def get_monthly_balance_by_user(username: str, month: Optional[int] = 1, year: Optional[int] = 2020):
+    if JWTBearer.authenticated_username != username:
+        raise credentials_exception
+    # bank_accounts_list = await get_bank_accounts_list_by_username(username)
+    # monthly_balance = await get_monthly_balance(bank_accounts_list, year, month)
+    return True 
